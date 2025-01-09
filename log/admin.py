dc0924995cbb80110657e5fd75608438adcf4003
alirg1999 <ali.programer.1999@gmail.com>
2025-01-09 21:20:25 +0330

    feat : add admin

--- log/admin.py
+++ log/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import ErrorLog, RestLog
+
+
+@admin.register(ErrorLog)
+class ErrorLogAdmin(admin.ModelAdmin):
+    list_display = ("timestamp", "user", "error_message")
+    search_fields = ("error_message",)
+
+
+@admin.register(RestLog)
+class RestLogAdmin(admin.ModelAdmin):
+    list_display = ("timestamp", "user", "action")
+    search_fields = ("action",)
