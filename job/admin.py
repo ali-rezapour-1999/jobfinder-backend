8d2abb6ed9c86141ba3920bc3334ea71989c0728
alirg1999 <ali.programer.1999@gmail.com>
2025-01-10 13:01:33 +0330

    fix: admin panel job

--- job/admin.py
+++ job/admin.py
@@ -1,16 +1,23 @@
 from django.contrib import admin
 
-from job.models import Job, SkillNeeded
+from job.models import Job, JobOptions, SkillNeeded
+
 
 @admin.register(Job)
 class JobAdmin(admin.ModelAdmin):
-    list_display = ('slug_id', 'title', 'user')
-    search_fields = ('title', 'slug_id')
-    list_filter = ('user',)
+    list_display = ("slug_id", "title", "user")
+    search_fields = ("title", "slug_id")
+    list_filter = ("user",)
 
 
 @admin.register(SkillNeeded)
 class SkillNeededAdmin(admin.ModelAdmin):
-    list_display = ('slug_id', 'title', 'level')
-    search_fields = ('title', 'slug_id')
-    list_filter = ('level',)
+    list_display = ("slug_id", "title", "level")
+    search_fields = ("title", "slug_id")
+    list_filter = ("level",)
+
+
+@admin.register((JobOptions))
+class JobOptionsAdmin(admin.ModelAdmin):
+    list_display = ("slug_id", "title")
+    search_fields = ("title", "slug_id")
--- job/models.py
+++ job/models.py
@@ -1,35 +1,42 @@
 from django.contrib.auth.models import models
-from django.core.validators import MinValueValidator, MaxValueValidator
+from django.core.validators import MaxValueValidator, MinValueValidator
+
 from core.utils import generate_unique_id
-from user.models import BaseModel ,CustomUser 
+from user.models import BaseModel, CustomUser
+
 
 class Job(BaseModel):
     slug_id = models.CharField(max_length=8, unique=True, default=generate_unique_id)
-    user = models.ForeignKey(CustomUser , on_delete=models.CASCADE ,related_name="user") 
-    title = models.CharField(max_length=255 ,null=False , blank=True)
-    main_title = models.CharField(max_length=255 ,null=False , blank=True)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="user")
+    title = models.CharField(max_length=255, null=False, blank=True)
+    main_title = models.CharField(max_length=255, null=False, blank=True)
     job_image = models.ImageField(upload_to="job_images/", null=True, blank=True)
     desciption = models.TextField()
 
     def __str__(self):
-        return f"{self.title} and {self.user}" 
+        return f"{self.title} and {self.user}"
 
 
 class SkillNeeded(BaseModel):
     slug_id = models.CharField(max_length=8, unique=True, default=generate_unique_id)
-    job = models.ManyToManyField(Job , related_name="job_skillneeded")
-    title= models.CharField(max_length=255 , null=True , blank=True)
-    level = models.DecimalField( max_digits=5, decimal_places=1, validators=[MinValueValidator(0), MaxValueValidator(100)]) 
+    job = models.ManyToManyField(Job, related_name="job_skillneeded")
+    title = models.CharField(max_length=255, null=True, blank=True)
+    level = models.DecimalField(
+        max_digits=5,
+        decimal_places=1,
+        validators=[MinValueValidator(0), MaxValueValidator(100)],
+    )
     description = models.TextField()
 
     def __str__(self):
-        return f"{self.title} and {self.job}" 
+        return f"{self.title} and {self.job}"
 
 
 class JobOptions(BaseModel):
-    job = models.ManyToManyField(Job , related_name="job_options")
-    title= models.CharField(max_length=255)
+    slug_id = models.CharField(max_length=8, unique=True, default=generate_unique_id)
+    job = models.ManyToManyField(Job, related_name="job_options")
+    title = models.CharField(max_length=255)
     description = models.TextField()
 
     def __str__(self):
-        return f"{self.title} and {self.job}" 
+        return f"{self.title} and {self.job}"
